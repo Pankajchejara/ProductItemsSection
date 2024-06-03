2e55306efe8c5e41d2109250cf8421d0a0bb07a3
pankajchejara <pankajkumarchejara119@gmail.com>
2024-06-03 11:06:35 +0530

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        'xs': '250px',  // Custom breakpoint for 250px screens
+        'sm': '640px',  // Small screens (default)
+        'md': '768px',  // Medium screens (default)
+        'lg': '1024px', // Large screens (default)
+        'xl': '1280px', // Extra large screens (default)
+        '2xl': '1536px', // 2XL screens (default)
+      },
     },
   },
   plugins: [],
